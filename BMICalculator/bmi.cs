5f722e8fc35528c2102b36632b9712d6d81dfb36
Declan Thomas <x00131110@mytudublin.ie>
2019-12-08 15:25:47 +0000

    Added CA Requirements

--- BMICalculator/bmi.cs
+++ BMICalculator/bmi.cs
@@ -1,6 +1,5 @@
 ﻿// model classes for BMI calculator
 // GC
-//
 
 using System;
 using System.ComponentModel.DataAnnotations;
@@ -8,6 +7,7 @@ using System.ComponentModel.DataAnnotations;
 namespace BMICalculator
 {
     public enum BMICategory { Underweight, Normal, Overweight, Obese };
+    public enum CalorieRange { Low, Average, High, Excessive };
 
     public class BMI
     {
@@ -18,6 +18,13 @@ namespace BMICalculator
         // conversion factors from imperial to metric
         const double PoundsToKgs = 0.453592;
         const double InchestoMetres = 0.0254;
+
+        [Display(Name = "Gender")]
+        public String PersonGender { get; set; }
+
+        [Display(Name = "Calories Per Day")]
+        [Range(0, 10000, ErrorMessage = "Calorie intake must be between 0 and 10000")]
+        public int CaloriesIntake { get; set; }
 
         [Display(Name = "Weight - Stones")]
         [Range(5, 50, ErrorMessage = "Stones must be between 5 and 50")]                              // max 50 stone
@@ -84,6 +91,46 @@ namespace BMICalculator
                 }
             }
         }
+
+        public CalorieRange CalorieRange
+        {
+            get
+            {
+                int calories = this.CaloriesIntake;
+
+                if (this.PersonGender == "male" && calories >= 0 && calories < 2000)
+                {
+                    return CalorieRange.Low;
+                }
+                else if (this.PersonGender == "female" && calories >= 0 && calories < 1700)
+                {
+                    return CalorieRange.Low;
+                }
+                else if (this.PersonGender == "male" && calories >= 2000 && calories < 3000)
+                {
+                    return CalorieRange.Average;
+                }
+                else if (this.PersonGender == "female" && calories >= 1700 && calories < 2600)
+                {
+                    return CalorieRange.Average;
+                }
+                else if (this.PersonGender == "male" && calories >= 3000 && calories < 4000)
+                {
+                    return CalorieRange.High;
+                }
+                else if (this.PersonGender == "female" && calories >= 2600 && calories < 3500)
+                {
+                    return CalorieRange.High;
+                }
+                else if (this.PersonGender == "male" && calories >= 4000 && calories < 10000)
+                {
+                    return CalorieRange.Excessive;
+                }
+                else 
+                {
+                    return CalorieRange.Excessive;
+                }
+            }
+        }
     }
 }
-
--- BMIUnitTestProject/UnitTest1.cs
+++ BMIUnitTestProject/UnitTest1.cs
@@ -13,5 +13,216 @@ namespace BMIUnitTestProject
             BMI bmi = new BMI() { WeightStones = 12, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
             Assert.AreEqual(bmi.BMICategory, BMICategory.Normal);
         }
+
+
+
+        [TestMethod]
+        public void TestBMIValue_ValidValuesEntered_HappyPath()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.WeightStones = 10;
+            bmi.WeightPounds = 10;
+            bmi.HeightInches = 10;
+            bmi.HeightFeet = 5;
+
+            //Act
+            string expectedResult = "21.5225205960616";
+
+            //Assert
+            Assert.AreEqual(expectedResult, bmi.BMIValue.ToString());
+        }
+
+        [TestMethod]
+        public void TestBMICategory_BMIValueIsUnderweight_ReturnUnderweight()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.WeightStones = 5;
+            bmi.WeightPounds = 5;
+            bmi.HeightInches = 10;
+            bmi.HeightFeet = 7;
+
+            //Act
+            BMICategory expectedResult = bmi.BMICategory;
+
+            //Assert
+            Assert.AreEqual(BMICategory.Underweight, expectedResult);
+        }
+
+
+        [TestMethod]
+        public void TestBMICategory_BMIValueIsNormal_ReturnNormal()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.WeightStones = 10;
+            bmi.WeightPounds = 5;
+            bmi.HeightInches = 10;
+            bmi.HeightFeet = 5;
+
+            //Act
+            BMICategory expectedResult = bmi.BMICategory;
+
+            //Assert
+            Assert.AreEqual(BMICategory.Normal, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestBMICategory_BMIValueIsOverweight_ReturnOverweight()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.WeightStones = 6;
+            bmi.WeightPounds = 10;
+            bmi.HeightInches = 1;
+            bmi.HeightFeet = 4;
+
+            //Act
+            BMICategory expectedResult = bmi.BMICategory;
+
+            //Assert
+            Assert.AreEqual(BMICategory.Overweight, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestBMICategory_BMIValueIsObese_ReturnObese()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.WeightStones = 20;
+            bmi.WeightPounds = 10;
+            bmi.HeightInches = 10;
+            bmi.HeightFeet = 5;
+
+            //Act
+            BMICategory expectedResult = bmi.BMICategory;
+
+            //Assert
+            Assert.AreEqual(BMICategory.Obese, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestCalorieRange_GenderIsMaleCaloriesAreLow_ReturnLow()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.PersonGender = "male";
+            bmi.CaloriesIntake = 1800;
+
+            //Act
+            CalorieRange expectedResult = bmi.CalorieRange;
+
+            //Assert
+            Assert.AreEqual(CalorieRange.Low, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestCalorieRange_GenderIsFemaleCaloriesAreLow_ReturnLow()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.PersonGender = "female";
+            bmi.CaloriesIntake = 1500;
+
+            //Act
+            CalorieRange expectedResult = bmi.CalorieRange;
+
+            //Assert
+            Assert.AreEqual(CalorieRange.Low, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestCalorieRange_GenderIsMaleCaloriesAreAverage_ReturnAverage()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.PersonGender = "male";
+            bmi.CaloriesIntake = 2500;
+
+            //Act
+            CalorieRange expectedResult = bmi.CalorieRange;
+
+            //Assert
+            Assert.AreEqual(CalorieRange.Average, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestCalorieRange_GenderIsFemaleCaloriesAreAverage_ReturnAverage()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.PersonGender = "female";
+            bmi.CaloriesIntake = 2200;
+
+            //Act
+            CalorieRange expectedResult = bmi.CalorieRange;
+
+            //Assert
+            Assert.AreEqual(CalorieRange.Average, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestCalorieRange_GenderIsMaleCaloriesAreHigh_ReturnHigh()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.PersonGender = "male";
+            bmi.CaloriesIntake = 3500;
+
+            //Act
+            CalorieRange expectedResult = bmi.CalorieRange;
+
+            //Assert
+            Assert.AreEqual(CalorieRange.High, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestCalorieRange_GenderIsFemaleCaloriesAreHigh_ReturnHigh()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.PersonGender = "female";
+            bmi.CaloriesIntake = 3200;
+
+            //Act
+            CalorieRange expectedResult = bmi.CalorieRange;
+
+            //Assert
+            Assert.AreEqual(CalorieRange.High, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestCalorieRange_GenderIsMaleCaloriesAreExcessive_ReturnExcessive()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.PersonGender = "male";
+            bmi.CaloriesIntake = 5000;
+
+            //Act
+            CalorieRange expectedResult = bmi.CalorieRange;
+
+            //Assert
+            Assert.AreEqual(CalorieRange.Excessive, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestCalorieRange_GenderIsFemaleCaloriesAreExcessive_ReturnExcessive()
+        {
+            //Arrange
+            BMI bmi = new BMI();
+            bmi.PersonGender = "female";
+            bmi.CaloriesIntake = 5000;
+
+            //Act
+            CalorieRange expectedResult = bmi.CalorieRange;
+
+            //Assert
+            Assert.AreEqual(CalorieRange.Excessive, expectedResult);
+
+
+        }
+
     }
 }
